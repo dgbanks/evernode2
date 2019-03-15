46426fe3761e811fd19f487ef686b4a0690c5e88
David <davidgbanks91@gmail.com>
2019-03-15 18:23:51 -0400

    Login component with Google oAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "antd/dist/antd.less";
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+import Login from "components/Login";
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Login />
     );
   }
 }
---
+++ client/src/components/Login/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import styled from "styled-components";
+
+class Login extends React.Component {
+  onSuccess = ({ code }) => {
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/login`, {
+      method: "POST",
+      body: JSON.stringify({ code }),
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Cell>
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            responseType="code"
+            scope="openid email"
+            accessType="offline"
+            onSuccess={this.onSuccess}
+          />
+        </Cell>
+      </Grid>
+    )
+  }
+}
+
+export default Login;
+
+const Grid = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 25% 50% 25%;
+  grid-template-rows: 25% 25% 50%;
+`;
+
+const Cell = styled.div`
+  grid-area: 2 / 2;
+  place-self: center;
+`;
