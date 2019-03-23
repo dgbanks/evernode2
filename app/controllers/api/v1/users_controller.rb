acfef8aba08a0a93f423c153902b076422f54377
David <davidgbanks91@gmail.com>
2019-03-23 19:26:23 -0400

    Users controller and show route

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V1
+    class UsersController < ApiController
+      def show
+        debugger
+        current_user
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      post "/login", to: "authentication#login" 
+      post "/login", to: "authentication#login"
+      resources :users, only: [:show]
     end
   end
 end
