d503efcf08bd8e1154232bd49e28a7f953126212
David <davidgbanks91@gmail.com>
2019-03-25 00:47:34 -0400

    Home screen with ant design components

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import { Button } from "antd";
+import { Layout, Typography, Dropdown, Menu, Spin, Icon } from "antd";
 import styled from "styled-components";
 import { logout } from "actions/authentication_actions";
 import { fetchCurrentUser } from "actions/current_user_actions";
+
+const Header = styled(Layout.Header)`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Title = styled(Typography.Title)`
+  color: white !important;
+  line-height: unset !important;
+  margin: 0px !important;
+`;
+
+const UserName = styled.span`
+  font-size: 20px;
+  margin-right:5px;
+`;
 
 class Home extends React.Component {
   constructor(props) {
@@ -15,14 +31,36 @@ class Home extends React.Component {
   }
 
   render() {
+    const { currentUser } = this.props;
+
+    if (currentUser.fetching || !currentUser.data) {
+      return (
+        <Layout style={{ height: "100vh", justifyContent: "center" }}>
+          <Spin indicator={<Icon type="loading" spin style={{ fontSize: 75 }} />} />
+        </Layout>
+      )
+    }
+
     return (
-      <Grid>
-        <Cell>
-          <Button type="primary" onClick={this.props.logout}>
-            Logout
-          </Button>
-        </Cell>
-      </Grid>
+      <Layout style={{ height: "100vh" }}>
+      <Header>
+        <Title>evernode</Title>
+        <Title>
+          <Dropdown overlay={<Menu>
+            <Menu.Item onClick={this.props.logout}>Logout</Menu.Item>
+          </Menu>}>
+            <a className="ant-dropdown-link" href="#">
+              <UserName>{currentUser.data.first_name}</UserName>
+              <Icon type="down" style={{ fontSize:15 }} />
+            </a>
+          </Dropdown>
+        </Title>
+      </Header>
+      <Layout style={{ height: "calc(100vh - 112px)" }}>
+      </Layout>
+      <Layout.Footer>
+      </Layout.Footer>
+      </Layout>
     );
   }
 }
@@ -40,15 +78,3 @@ export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
 )(Home));
-
-const Grid = styled.div`
-  height: 100vh;
-  display: grid;
-  grid-template-columns: 25% 50% 25%;
-  grid-template-rows: 25% 25% 50%;
-`;
-
-const Cell = styled.div`
-  grid-area: 2 / 2;
-  place-self: center;
-`;
