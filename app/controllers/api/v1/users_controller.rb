9bf40a507f2e5679462a4b7a01dba17f1424dc37
David <davidgbanks91@gmail.com>
2019-03-30 18:50:59 -0400

    Refactor current_user json response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,28 @@
 module Api
   module V1
     class UsersController < ApiController
-      include UserResponseTemplates
-
       def show
-        render_ok({ user: user_data(current_user) }) if current_user
+        render_ok({ user: user_json }) if current_user
+      end
+
+      private
+
+      def user_json
+        {
+          email: current_user.email,
+          first_name: current_user.first_name,
+          last_name: current_user.last_name,
+          canvas: canvases_json
+        }
+      end
+
+      def canvases_json
+        current_user.canvases.map do |canvas|
+          {
+            id: canvas.id,
+            title: canvas.title,
+          }
+        end
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
   private
 
   def current_user
-    if request.headers["Authorization"].present?
+    @current_user ||= if request.headers["Authorization"].present?
       valid_token = JSONWebToken.decode(request.headers["Authorization"])
       valid_token ? User.find(valid_token[:user_id]) : nil
     end
