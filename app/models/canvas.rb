0e8c21a00871e9302012f222d33bb8ef4fd8900b
David <davidgbanks91@gmail.com>
2019-03-30 13:00:11 -0400

    Canvas edges through nodes association

--- app/models/canvas.rb
+++ app/models/canvas.rb
@@ -2,4 +2,5 @@ class Canvas < ApplicationRecord
   validates_presence_of :title, :user_id
   belongs_to :owner, foreign_key: :user_id, class_name: :User
   has_many :nodes
+  has_many :edges, through: :nodes, source: :edges
 end
