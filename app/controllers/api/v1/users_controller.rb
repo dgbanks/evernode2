ef584d8d5d5099c03bd0c238daf9cafeab34f120
David <davidgbanks91@gmail.com>
2019-03-31 11:07:13 -0400

    Fix current_user response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,7 @@ module Api
           email: current_user.email,
           first_name: current_user.first_name,
           last_name: current_user.last_name,
-          canvas: canvases_json
+          canvases: canvases_json
         }
       end
 
