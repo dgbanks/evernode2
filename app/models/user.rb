8f632b998221fbb7af588a5042b080a4d98e21a9
David <davidgbanks91@gmail.com>
2019-04-27 11:15:45 -0400

    User canvases dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :email
   validates_uniqueness_of :email
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-  has_many :canvases
+  has_many :canvases, dependent: :destroy
 
   def name
     first_name + " " + last_name
