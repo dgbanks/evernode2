419285799974a6f6f09000feeac9cf7a8741ca90
David <davidgbanks91@gmail.com>
2019-04-27 00:46:21 -0400

    CanvasesController and endpoints

---
+++ app/controllers/api/v1/canvases_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class CanvasesController < ApiController
+      def create
+        canvas
+      end
+
+      def show
+
+      end
+
+      def update
+
+      end
+
+      def destroy
+
+      end
+
+      private
+
+      def canvas_json
+
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post "/login", to: "authentication#login"
       resources :users, only: [:show]
+      resources :canvases, only: [:create, :show, :update, :destroy]
     end
   end
 end
